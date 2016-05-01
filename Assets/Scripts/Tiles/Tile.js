c6d054b57c3a1786782cdd41099f28233cbd36ad
Topher <tophermade@gmail.com>
2016-04-30 21:04:04 -0600

    added tile drop check

--- Assets/Scripts/Tiles/Tile.js
+++ Assets/Scripts/Tiles/Tile.js
@@ -5,5 +5,8 @@ function Start () {
 }
 
 function Update () {
-
+	// var v3 = Input.mousePosition;
+	// v3.z = 10.0;
+	// v3 = Camera.main.ScreenToWorldPoint(v3);
+	// transform.position = v3;
 }
---
+++ Assets/Scripts/Tiles/TilePicker.js
@@ -0,0 +1,44 @@
+﻿#pragma strict
+
+var gameBoard 			: GameObject;
+
+var tileToDrag 			: GameObject;
+
+function Start () {
+
+}
+
+function Update () {
+
+	if(Input.GetMouseButtonDown(0)){
+		var hit : RaycastHit;
+		var ray : Ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+		if (Physics.Raycast (ray,hit)){
+			print(hit.transform.gameObject.name);
+			tileToDrag = hit.transform.gameObject;
+		}
+	}
+
+	if(Input.GetMouseButtonUp(0)){
+		if(tileToDrag){
+			print("trying drop cast");
+			var hitBelow		: RaycastHit;
+			var hitObject 		: GameObject;
+			var castTo 			: Vector3 = (Vector3(tileToDrag.transform.position.x, tileToDrag.transform.position.y, tileToDrag.transform.position.z + 30) - tileToDrag.transform.position ).normalized;
+			if (Physics.Raycast(transform.position, castTo, hitBelow, 30)){
+				hitObject 		= hitBelow.transform.gameObject;
+				print("hit object " + hitObject.name);
+			}
+		}
+
+		tileToDrag 	= null;
+	}
+
+	if(tileToDrag){
+		var pos = Input.mousePosition;
+		pos.z = 10.0;
+		pos = Camera.main.ScreenToWorldPoint(pos);
+		tileToDrag.transform.position = pos;
+	}
+
+}
