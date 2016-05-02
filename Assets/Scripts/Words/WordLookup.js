301b2a5b5de85d32bfb95ce0d31eab34589670ab
Topher <tophermade@gmail.com>
2016-05-01 22:33:28 -0600

    added dictionary lookup

---
+++ Assets/Scripts/Words/WordLookup.js
@@ -0,0 +1,31 @@
+﻿import System.Collections.Generic;
+import System.Linq;
+
+var myDictionary 	: Dictionary.<String, String>;
+var wordToCheck 	: String;
+
+function CheckWord(word : String) {
+	print(Time.time);
+	if(myDictionary.ContainsKey(word)){
+		Debug.Log(word + " is a valid word");		
+	} else if(!myDictionary.ContainsKey(word)){
+		Debug.Log(word + " is NOT a valid word");		
+	}    
+	print(Time.time);
+}
+
+function Awake(){
+	var MytextAsset = Resources.Load("dictionary", typeof(TextAsset))  as TextAsset;
+	myDictionary = MytextAsset.text.Split("\n"[0]).ToDictionary(function(w){
+		return w;
+	}); 
+}
+
+function Start(){
+	yield WaitForSeconds(1);
+	CheckWord(wordToCheck);
+}
+
+function Update(){
+	
+}
