386e2ba17af0125b56346343de555493da39df6c
Topher <tophermade@gmail.com>
2016-05-11 21:10:13 -0600

    started on level select scene

--- Assets/Scripts/SceneSpecific/Scene-Index.js
+++ Assets/Scripts/SceneSpecific/Scene-Index.js
@@ -4,14 +4,13 @@ var splashLogo 			: GameObject;
 var transitionMaker 	: GameObject;
 
 
-
 function Start () {
 	transitionMaker.SetActive(true);
 	splashLogo.SetActive(true);
 
 	yield WaitForSeconds(2);
 	splashLogo.GetComponent(Animator).enabled = true;
-	
+
 	yield WaitForSeconds(.15);
 	transitionMaker.SendMessage("Setup", false);
 	transitionMaker.SendMessage("ReadyOut");
